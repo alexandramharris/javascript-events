2f7c346492b7db956e9a55b8f0a67f6edd15c629
Alexandra Harris <harrisalexmarie@gmail.com>
2022-11-08 19:54:55 -0500

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,8 @@
 //First event
-
 const image = document.querySelector('img');
 
-let leaf = "images/leaf-icon.svg";
 let tree = "images/tree.jpg";
+let trees = "images/trees.jpg";
 
 // when we use e.target it means "the target
 // of the event" - the event here is the
@@ -11,15 +10,35 @@ let tree = "images/tree.jpg";
 // target is the element preceding the dot -
 // in this case, the variable image
 
-image.onmouseenter = (e) => {
+image.onclick = (e) => {
 	// change image
    e.target.setAttribute('src', tree);
 };
 
-image.onmouseleave = (e) => {
+image.onclick = (e) => {
 // change image
-   e.target.setAttribute('src', leaf);
+   e.target.setAttribute('src', trees);
 };
 
 
 // Second event
+const image2 = document.querySelector('#special');
+
+let tu = "images/tu.jpg";
+let dogs = "images/dogs.jpg";
+
+// when we use e.target it means "the target
+// of the event" - the event here is the
+// onmouseenter or the onmouseleave, and the
+// target is the element preceding the dot -
+// in this case, the variable image
+
+image2.onmouseover = (e) => {
+	// change image
+   e.target.setAttribute('src', dogs);
+};
+
+image2.onmouseleave = (e) => {
+// change image
+   e.target.setAttribute('src', tu);
+};
